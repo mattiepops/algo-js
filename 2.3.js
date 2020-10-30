49ec1c5df517c1ee451ee6baabbc47814141b2fd
Matt <Matt@Mateis-MacBook-Pro.local>
2020-10-30 10:29:45 +0100

    edited the for loop, so it runs 50 times

--- 2.3.js
+++ 2.3.js
@@ -7,9 +7,14 @@ while (i++ <= 100) {
 }
 
 
-for(i=1; i<=100; i++){
+for(i=1; i<=100;){
     if(i % 2 == 0){
       console.log(i);
     }
+    i++;
+    if(i % 2 == 0){
+        console.log(i);
+      }
+      i++;
   }
   
